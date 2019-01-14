eec83641abaa70c2e59196ab1cf04fab3b2254df
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-14 20:04:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,16 @@ function whileLoop(n) {
   {console.log(n--)}
   return "done"
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
+}
+
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
